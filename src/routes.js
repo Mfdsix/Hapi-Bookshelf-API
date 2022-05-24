b4a945d6f23bd7aa0fb719e54b590e8ac8dc437b
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2022-05-24 11:23:10 +0700

    add book routes

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,37 @@
-const routes = []
+const {
+    getAllBookshelfsHandler,
+    getSpecificBookshelfHandler,
+    createBookshelfHandler,
+    updateBookshelfHandler,
+    deleteBookshelfHandler
+} = require('./handler')
+
+const routes = [
+    {
+        method: 'GET',
+        path: '/books',
+        handler: getAllBookshelfsHandler
+    },
+    {
+        method: 'GET',
+        path: '/books/{id}',
+        handler: getSpecificBookshelfHandler
+    },
+    {
+        method: 'POST',
+        path: '/books',
+        handler: createBookshelfHandler
+    },
+    {
+        method: 'PUT',
+        path: '/books/{id}',
+        handler: updateBookshelfHandler
+    },
+    {
+        method: 'DELETE',
+        path: '/books/{id}',
+        handler: deleteBookshelfHandler
+    },
+]
 
 module.exports = routes
