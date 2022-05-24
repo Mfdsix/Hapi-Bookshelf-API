17c044f733d9577dc034251a3fec472fcfe4a1a0
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2022-05-24 11:20:17 +0700

    add handler functions

---
+++ src/handler.js
@@ -0,0 +1,20 @@
+const bookshelfs = require('./books')
+const { responseSuccess, responseError } = require('./response')
+
+const getAllBookshelfsHandler = (req, h) => { }
+
+const getSpecificBookshelfHandler = (req, h) => { }
+
+const createBookshelfHandler = (req, h) => { }
+
+const updateBookshelfHandler = (req, h) => { }
+
+const deleteBookshelfHandler = (req, h) => { }
+
+module.exports = {
+    getAllBookshelfsHandler,
+    getSpecificBookshelfHandler,
+    createBookshelfHandler,
+    updateBookshelfHandler,
+    deleteBookshelfHandler
+}
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const Hapi = require('@hapi/hapi')
+const routes = require('./routes')
 
 const port = 3000
 const host = process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost'
@@ -6,9 +7,15 @@ const host = process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost'
 const init = (async ({ host, port }) => {
     const server = Hapi.server({
         host: host,
-        port: port
+        port: port,
+        routes: {
+            cors: {
+                origin: ['*']
+            }
+        }
     })
 
+    server.route(routes)
     server.start()
     console.log(`Server Started at ${server.info.url}`)
 })
