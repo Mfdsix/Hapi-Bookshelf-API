21cf6ef1d5f08f78fe2162ff7cadb364c7f9b989
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2022-05-24 10:45:18 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const Hapi = require('@hapi/hapi')
+
+const port = 3000
+const host = process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost'
+
+const init = (async ({ host, port }) => {
+    const server = Hapi.server({
+        host: host,
+        port: port
+    })
+
+    server.start()
+    console.log(`Server Started at ${server.info.url}`)
+})
+
+init()
