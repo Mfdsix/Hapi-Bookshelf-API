5956b8fa34d17253f213a8246590305beed98301
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2022-05-24 16:21:37 +0700

    fix eslint

--- src/response.js
+++ src/response.js
@@ -1,19 +1,19 @@
-const responseSuccess = (data, message = "Success") => {
-    return {
-        status: 'success',
-        message: message,
-        data: data
-    }
+const responseSuccess = (data, message = 'Success') => {
+	return {
+		status: 'success',
+		message: message,
+		data: data
+	}
 }
 
-const responseError = (message = "Error") => {
-    return {
-        status: 'fail',
-        message: message,
-    }
+const responseError = (message = 'Error') => {
+	return {
+		status: 'fail',
+		message: message,
+	}
 }
 
 module.exports = {
-    responseSuccess,
-    responseError
+	responseSuccess,
+	responseError
 }
--- src/server.js
+++ src/server.js
@@ -5,22 +5,22 @@ const port = 5000
 const host = process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost'
 
 const init = async ({ host, port }) => {
-    const server = Hapi.server({
-        host: host,
-        port: port,
-        routes: {
-            cors: {
-                origin: ['*']
-            }
-        }
-    })
+	const server = Hapi.server({
+		host: host,
+		port: port,
+		routes: {
+			cors: {
+				origin: ['*']
+			}
+		}
+	})
 
-    server.route(routes)
-    server.start()
-    console.log(`Server Started at ${server.info.uri}`)
+	server.route(routes)
+	server.start()
+	console.log(`Server Started at ${server.info.uri}`)
 }
 
 init({
-    host,
-    port,
+	host,
+	port,
 })
